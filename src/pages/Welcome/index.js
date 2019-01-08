c4e3a41e8e645d74d37aa400f52659d8b9744a4a
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2019-01-08 19:27:45 -0200

    feat: Stylize welcome page

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
 
-const Welcome = () => <View />;
+import Styles from './styles';
+
+const Welcome = () => (
+  <View style={Styles.container}>
+    <StatusBar barStyle="light-content" />
+    <Text style={Styles.title}>Welcome</Text>
+    <Text style={Styles.text}>To continue we need you to tell your GitHub user.</Text>
+
+    <View style={Styles.form}>
+      <TextInput
+        style={Styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Enter your username"
+        underlineColorAndroid="transparent"
+      />
+
+      <TouchableOpacity style={Styles.button} onPress={() => {}}>
+        <Text style={Styles.buttonText}>Proceed</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
 
 export default Welcome;
