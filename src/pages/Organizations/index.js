f9818111ae2ea180003649b5330bcd8949e1cecd
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2019-01-08 21:57:48 -0200

    refactor: Adjust pages with tab navigation attributes

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -7,20 +7,20 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
 
-const Repositories = () => (
+const Organizations = () => (
   <View>
-    <Header title="Repositories" />
+    <Header title="Organizations" />
   </View>
 );
 
-const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
 
 TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Repositories.navigationOptions = {
+Organizations.navigationOptions = {
   tabBarIcon: TabIcon,
 };
 
-export default Repositories;
+export default Organizations;
